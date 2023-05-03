420b070866f3fdfc4b363ab57281ff56ec33631c
atx <mouta.luis13@gmail.com>
2023-05-03 15:55:34 +0200

    First version

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+games { 'common' }
+
+author { 'Cloudev' }
+description { 'Another Cloudev loading screen' }
+version '0.2.0'
+
+loadscreen 'load.html'
+
+files {
+    'loadscreen.html',
+    'css/style.css',
+    'img/logo.png',
+    'js/script.js',
+}
